4bd8e33f6a55e4e61ca8ad8c825f90434e3e8fbc
dhavalbothra <57654403+dhavalbothra@users.noreply.github.com>
2020-05-27 15:56:55 +0530

    Update BigDecimal.hpp

--- BigDecimal.hpp
+++ BigDecimal.hpp
@@ -1,167 +1,104 @@
 #include<iostream>
 #include<malloc.h>
 #include<cstring>
-#include<bits/stdc++.h>
+#include<vector>
 #include<cstdlib>
 using namespace std;
 
 class BigDecimal
 {
 public:
-        int Int_part;
-        int prec = 100;
-        int len = prec
-        bool sign = false;
-        vector<uint8_t> Dec_part;
+        long Int_part;
+        int prec = 10;
+        bool sign = false; // false indicates that integer is positive
+        vector<int> Dec_part;
 
         BigDecimal(){};
-        BigDecimal(int n){memset(Dec_part,0,prec*sizeof(uint8_t));Int_part = abs(n);if{abs(n) == n}{sign = true;}}
-        BigDecimal(long n){memset(Dec_part,0,prec*sizeof(uint8_t));Int_part = abs(n);if{abs(n) == n}{sign = true;}}
+        // Memset is not used for vectors or any other container it is just used for arrays
+        BigDecimal(int n){Int_part = abs(n);Dec_part.resize(prec,0);if(n<0){sign = true;}}
+        BigDecimal(long n){Int_part = abs(n);Dec_part.resize(prec,0);if(n<0){sign = true;}}
+
+        void set_precision(int x){
+            Dec_part.resize(x,0);
+        }
 
         BigDecimal operator + (BigDecimal othr){return add(*this,othr);}
-        BigDecimal operator - (BigDecimal othr){return sub(*this,othr);}
-        BigDecimal operator / (long n){return divide(*this,n);}
-        BigDecimal operator * (long n){return mul(*this,n);}
+        // BigDecimal operator - (BigDecimal othr){return sub(*this,othr);}
+        // BigDecimal operator / (long n){return divide(*this,n);}
+        // BigDecimal operator * (long n){return mul(*this,n);}
+        bool operator < (BigDecimal othr){
+            if(Int_part >= othr.Int_part){
+                return false;
+            }
+            for(int i=0;i<min(prec,othr.prec);i++){
+                if(Dec_part[i] >= othr.Dec_part[i]){
+                    return false;
+                }
+            }
+            if(prec > othr.prec){
+                for(int i=othr.prec - 1;i<prec;i++){
+                    if(Dec_part[i] > 0)return false;
+                }
+            }
+            else{
+                for(int i=prec - 1;i<othr.prec;i++){
+                    if(othr.Dec_part[i] > 0)return false;
+                }
+            }
+	    return true;
+        }
+	bool operator >= ( BigDecimal othr){return !(*this < othr);}
 		BigDecimal add(BigDecimal a_, BigDecimal b_)
 		{
-			if(a_.sign && b_.sign)
+            // indicates either both are positive or negative
+			if(!(a_.sign ^ b_.sign))
 			{
-
+                int ans_prec = max(a_.prec,b_.prec);
 				BigDecimal ans(0);
-				vector<uint8_t> a = a_.Dec_part;
-				vector<uint8_t> b = b_.Dec_part;
-				int i;
-				int c
-				for(i=len-1;i>0;i--)
+                ans.set_precision(ans_prec);
+				vector<int> a = a_.Dec_part;
+				vector<int> b = b_.Dec_part;
+                if(a_.prec != b_.prec){
+                    for(int i=ans_prec - 1;i>=min(a_.prec,b_.prec);i--){
+                        if(a_.prec > b_.prec) ans.Dec_part[i] = a[i];
+                        else ans.Dec_part[i] = b[i];
+                    }
+                }
+				for(int i=min(a_.prec,b_.prec)-1;i>0;i--)
 				{
 					ans.Dec_part[i] += (a[i] + b[i])%10;
 					ans.Dec_part[i-1] += (a[i] + b[i])/10;
 				}
 				ans.Dec_part[0] += (a[0] + b[0]);
+                ans.Int_part += (ans.Dec_part[0]/10);
 				ans.Dec_part[0] = ans.Dec_part[0] % 10;
-				ans.Int_part = a_.Int_part + b_.Int_part + ans.Dec_part[0] / 10;
-				ans.sign = true;
+				ans.Int_part = a_.Int_part + b_.Int_part;
+				ans.sign = a_.sign & b_.sign;
 				return ans;
 			}
-
-			else if(a_.sign)
-            {
-                if(a_ > b_)
-                {
-                    BigDecimal ans(0);
-                    vector<uint8_t> a = a_.Dec_part;
-                    vector<uint8_t> b = b_.Dec_part;
-                    int i;
-                    for(i=len-1;i>0;i--)
-                    {
-                        if(a[i]>=b[i]){ ans.Dec_part[i] = a[i] - b[i]; }
-                        if(a[i]<b[i]) { ans.Dec_part[i] = 10 + a[i] - b[i]; a[i-1] = a[i-1] -1;}
-                    }
-                    if(a[0] >= b[0])
-                        ans.Dec_part[0] = a[0] - b[0];
-                        ans.Int_part = a_.Int_part - b_.Int_part;
-                    else
-                        ans.Dec_part[0] = a[0] - b[0] + 10;
-                        ans.Int_part = a_.Int_part - b_.Int_part -1;
-                    ans.sign = true;
-                    return ans;
-                }
-                else
-                {
-                    BigDecimal ans(0);
-                    vector<uint8_t> a = a_.Dec_part;
-                    vector<uint8_t> b = b_.Dec_part;
-                    int i;
-                    for(i=len-1;i>0;i--)
-                    {
-                        if(a[i]<=b[i]){ ans.Dec_part[i] = b[i] - a[i]; }
-                        if(a[i]>b[i]) { ans.Dec_part[i] = 10 + b[i] - a[i]; b[i-1] = b[i-1] -1;}
-                    }
-                    if(b[0] >= a[0])
-                        ans.Dec_part[0] = b[0] - a[0];
-                        ans.Int_part = b_.Int_part - a_.Int_part;
-                    else
-                        ans.Dec_part[0] = b[0] - a[0] + 10;
-                        ans.Int_part = b_.Int_part - a_.Int_part -1;
-                    ans.sign = false;
-                    return ans;
-                }
-            }
-
-            else if(b_.sign)
-            {
-                if(b_ > a_)
-                {
-                    BigDecimal ans(0);
-                    vector<uint8_t> a = a_.Dec_part;
-                    vector<uint8_t> b = b_.Dec_part;
-                    int i;
-                    for(i=len-1;i>0;i--)
-                    {
-                        if(a[i]<=b[i]){ ans.Dec_part[i] = b[i] - a[i]; }
-                        if(a[i]>b[i]) { ans.Dec_part[i] = 10 + b[i] - a[i]; b[i-1] = b[i-1] -1;}
-                    }
-                    if(b[0] >= a[0])
-                        ans.Dec_part[0] = b[0] - a[0];
-                        ans.Int_part = b_.Int_part - a_.Int_part;
-                    else
-                        ans.Dec_part[0] = b[0] - a[0] + 10;
-                        ans.Int_part = b_.Int_part - a_.Int_part -1;
-                    ans.sign = true;
-                    return ans;
-                }
-
-                else
-                {
-                    BigDecimal ans(0);
-                    vector<uint8_t> a = a_.Dec_part;
-                    vector<uint8_t> b = b_.Dec_part;
-                    int i;
-                    for(i=len-1;i>0;i--)
-                    {
-                        if(a[i]>=b[i]){ ans.Dec_part[i] = a[i] - b[i]; }
-                        if(a[i]<b[i]) { ans.Dec_part[i] = 10 + a[i] - b[i]; a[i-1] = a[i-1] -1;}
-                    }
-                    if(a[0] >= b[0])
-                        ans.Dec_part[0] = a[0] - b[0];
-                        ans.Int_part = a_.Int_part - b_.Int_part;
-                    else
-                        ans.Dec_part[0] = a[0] - b[0] + 10;
-                        ans.Int_part = a_.Int_part - b_.Int_part -1;
-                    ans.sign = false;
-                    return ans;
-                }
-            }
-			else
-			{
-
-				BigDecimal ans(0);
-				vector<uint8_t> a = a_.Dec_part;
-				vector<uint8_t> b = b_.Dec_part;
-				int i;
-				int c
-				for(i=len-1;i>0;i--)
-				{
-					ans.Dec_part[i] += (a[i] + b[i])%10;
-					ans.Dec_part[i-1] += (a[i] + b[i])/10;
-				}
-				ans.Dec_part[0] += (a[0] + b[0]);
-				ans.Dec_part[0] = ans.Dec_part[0] % 10;
-				ans.Int_part = a_.Int_part + b_.Int_part + ans.Dec_part[0] / 10;
-				ans.sign = false;
-				return ans;
-			}
+            return BigDecimal(0);
+            // both are of oposite signs
+            // else{
+            //     // if(a_ < b_){
+            //     //     return sub(b_,a_);
+            //     // }
+            //     // else{
+            //     //     return sub(a_,b_);
+            //     // }
+            // }
 		}
 
-        BigDecimal sub(BigDecimal a_, BigDecimal b_)
-        void print(BigDecimal num)
+        // BigDecimal sub(BigDecimal a_, BigDecimal b_){
+
+        // }
+        void print()
         {
-            if(sign == false)
-                cout << "-";
-            cout << Int_part + ".";
-            for(int i = 0;i < len;i++)
+            if(sign) cout << "-";
+            cout << Int_part<<".";
+            for(int i = 0;i < prec;i++)
             {
                 cout << Dec_part[i];
             }
+            cout<<endl;
         }
 };
