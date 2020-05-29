aeabbc8d94e0d644e90e554cc538e4d68b49baa3
dhavalbothra <57654403+dhavalbothra@users.noreply.github.com>
2020-05-29 14:44:31 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,9 @@
 int main(int argc, char** argv) {
 	
 	BigDecimal a = 4;
-	BigDecimal b;
-	for(int i=1;i<=1000;i++)
-	{
-		//b.divide(4,2*i+1);
-		b = BigDecimal(4) / (2*i+1);
-		if(i%2==1)
-		{
-			//a.sub(a,b);
-			a = a-b;
-		}
-		else
-		{
-			//a.add(a,b);
-			a = a+b;	
-		}
-	}
+	BigDecimal b = 3;
 	a.print();
+	b.print();
+	b = b + a;
+	b.print();
 }
